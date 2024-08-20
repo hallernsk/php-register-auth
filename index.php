493c3003eb87fa50a42442bc7ce3f4b3d20623cc
hallernsk <avladim@mail.ru>
2024-08-20 15:06:35 +0700

    add index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Главная страница</title>
+</head>
+<body>
+    <h3>Главная страница сайта</h3>
+    <p>Авторизуйтесь или зарегистрируйтесь, чтобы получить доступ к вашему профилю.</p>
 
-echo "Главная страница";
+    <a href="login.php"><button>Авторизация</button></a>
+    <a href="register.php"><button>Регистрация</button></a>
+</body>
+</html>
